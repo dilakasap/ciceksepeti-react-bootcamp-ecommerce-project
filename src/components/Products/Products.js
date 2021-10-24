68d297cbced636f5e5b19b353813539cc87b8f7b
dilakasap <dilakasap@gmail.com>
2021-10-24 14:26:17 +0300

    product categories

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { REQUEST_STATUS } from "../../helpers/Constants";
 import { getProducts } from "../../redux/actions/products";
 import "./Product.scss";
 
-function Products() {
+function Products({ selectedCategory }) {
+  const [filteredData, setFilteredData] = useState([]);
   const dispatch = (Products = useDispatch());
   useEffect(() => {
     dispatch(getProducts());
@@ -15,32 +16,42 @@ function Products() {
   const toProductDetails = (id) => {
     history.push("/products/" + id);
   };
+  
   return (
     <>
       {products.status === REQUEST_STATUS.PENDING && <div>Loading...</div>}
       {products.status === REQUEST_STATUS.SUCCESS && (
         <div className="product-container">
-          {products.data.map((item) => (
-            <div
-              onClick={() => toProductDetails(item.id)}
-              className="product-card"
-              key={item.id}
-            >
-              <div className="image">
-                <img src={item.imageUrl}></img>
-              </div>
-              <div className="top-info">
-                <div className="product-title">{item.brand.title}</div>
-                <div id="product-color">
-                  <p>
-                    <span>Renk: </span>
-                    {item.color.title}
-                  </p>
+          {products.data
+            .filter((product) => {
+              if (
+                selectedCategory === "" ||
+                product.category.id === selectedCategory
+              ) {
+                return product;
+              }
+            })
+            .map((item) => (
+              <div
+                onClick={() => toProductDetails(item.id)}
+                className="product-card"
+                key={item.id}
+              >
+                <div className="image">
+                  <img src={item.imageUrl}></img>
                 </div>
+                <div className="top-info">
+                  <div className="product-title">{item.brand.title}</div>
+                  <div id="product-color">
+                    <p>
+                      <span>Renk: </span>
+                      {item.color.title}
+                    </p>
+                  </div>
+                </div>
+                <div className="product-price">{item.price} TL</div>
               </div>
-              <div className="product-price">{item.price} TL</div>
-            </div>
-          ))}
+            ))}
         </div>
       )}
     </>
